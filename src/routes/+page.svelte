12504fafb29c9946a2d86d5ef96a524f1b06790d
ylast <y.last@student.science.ru.nl>
2023-08-03 14:32:41 +0200

    Formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,9 +54,8 @@
       </h2>
 
       <p class="main-text infoblock-text">
-        This application can be freely used by everyone! Its source code is
-        also available <i>(link pending)</i> so that anyone can inspect it or make
-        improvements!
+        This application can be freely used by everyone! Its source code is also
+        available <i>(link pending)</i> so that anyone can inspect it or make improvements!
       </p>
     </div>
 
