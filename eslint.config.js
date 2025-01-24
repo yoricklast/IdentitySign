25fce866a3a90af6c5180d3d79a68eeafcbc5f41
Vanessa Susewind <vasu@mail.uni-paderborn.de>
2025-01-24 22:21:38 +0100

    Migrate to eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import prettier from "eslint-config-prettier";
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+import ts from "typescript-eslint";
+
+export default ts.config(
+    js.configs.recommended,
+    ...ts.configs.recommended,
+    ...svelte.configs["flat/recommended"],
+    prettier,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
+    {
+        files: ["**/*.svelte"],
+
+        languageOptions: {
+            parser: svelte.parser,
+            parserOptions: {
+                parser: ts.parser,
+            },
+        },
+    },
+    {
+        ignores: ["build/", ".svelte-kit/", "dist/", "static/yivi/"],
+    },
+    {
+        plugins: {
+            "@typescript-eslint": ts.plugin,
+        },
+    },
+);
